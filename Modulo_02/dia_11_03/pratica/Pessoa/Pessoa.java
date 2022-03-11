0565be8af799533f69ddfeb8719ae4f0a9b5ff6c
Marcelo <marcelo.leite@mercadolivre.com>
2022-03-11 17:34:44 -0300

    [feat] exercicio 02 digital house

--- Modulo_02/dia_11_03/pratica/Pessoa/Pessoa.java
+++ Modulo_02/dia_11_03/pratica/Pessoa/Pessoa.java
@@ -9,6 +9,12 @@ public class Pessoa {
 
     }
 
+    public Pessoa(String nome, String id, int idade) {
+        this.nome = nome;
+        this.id = id;
+        this.idade = idade;
+    }
+
     public Pessoa(String id, String nome, int idade, float peso, float altura) {
         this.id = id;
         this.nome = nome;
