bdce5dfd05cd213ebf723f79db8c1246166c8eca
Marcelo <marcelo.leite@mercadolivre.com>
2022-03-11 17:49:58 -0300

    [feat] exercicio 04 e 05 digital house

--- Modulo_02/dia_11_03/pratica/Pessoa/Main.java
+++ Modulo_02/dia_11_03/pratica/Pessoa/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Pessoa pessoa1 = new Pessoa("Marcelo", 36, null);
     }
 }
--- Modulo_02/dia_11_03/pratica/Pessoa/Pessoa.java
+++ Modulo_02/dia_11_03/pratica/Pessoa/Pessoa.java
@@ -9,17 +9,38 @@ public class Pessoa {
 
     }
 
-    public Pessoa(String nome, String id, int idade) {
+    public Pessoa(String nome, int idade, String id) {
         this.nome = nome;
         this.id = id;
         this.idade = idade;
     }
 
-    public Pessoa(String id, String nome, int idade, float peso, float altura) {
+    public Pessoa(String nome, int idade, String id, float peso, float altura) {
         this.id = id;
         this.nome = nome;
         this.idade = idade;
         this.peso = peso;
         this.altura = altura;
+    }
+
+    public int calcularImc() {
+        float imc = (peso / (altura * altura));
+        if ( imc < 20 ) {
+            return -1;
+        } else if (imc >= 20 && imc < 25) {
+            return 0;
+        } else if (imc >= 25)  {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
+    public boolean ehMaiorIdade() {
+        if (idade >= 18) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
