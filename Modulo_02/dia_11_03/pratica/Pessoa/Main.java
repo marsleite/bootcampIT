8a9918c88df484253f95352d366a532f9b6a85b3
Marcelo <marcelo.leite@mercadolivre.com>
2022-03-11 17:59:31 -0300

    [feat] exercicio 06 digital house

--- Modulo_02/dia_11_03/pratica/Pessoa/Main.java
+++ Modulo_02/dia_11_03/pratica/Pessoa/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        Pessoa pessoa1 = new Pessoa("Marcelo", 36, null);
+        Pessoa pessoa = new Pessoa("Marcelo", 36, "1234501", 95.5f, 1.95f);
+        System.out.println("--------------------");
+        System.out.println("id: " + pessoa.id);
+        System.out.println("Nome: " + pessoa.nome);
+        System.out.println("Idade: " + pessoa.idade);
+        System.out.println("Peso: " + pessoa.peso);
+        System.out.println("Altura: " + pessoa.altura);
+        System.out.println("--------------------");
+
+        if (pessoa.calcularImc() == -1) {
+            System.out.println("Abaixo do peso");
+        } else if (pessoa.calcularImc() == 0) {
+            System.out.println("Peso Saudavel");
+        } else if (pessoa.calcularImc() == 1) {
+            System.out.println("Sobrepeso");
+        } else {
+            System.out.println("Obeso");
+        }
     }
 }
