680457258021718b9257366dd897044dab2e2f6f
Marcelo <marcelo.leite@mercadolivre.com>
2022-03-17 16:03:07 -0300

    [feat] pratica integradora 01 dia 17_03

---
+++ Modulo_03/dia_17_03/Pratica_integradora01/romanos/src/test/java/br/com/praticaIntegradora/romanos/RomanosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.praticaIntegradora.romanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
