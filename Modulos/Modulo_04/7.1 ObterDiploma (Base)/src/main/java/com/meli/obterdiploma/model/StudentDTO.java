2a9954484f4db1db7ddd84e6e503f9e4741372d6
Marcelo <marcelo.leite@mercadolivre.com>
2022-03-28 14:08:30 -0300

    [feat] exercicio de validacao primeira parte feita

--- Modulos/Modulo_04/7.1 ObterDiploma (Base)/src/main/java/com/meli/obterdiploma/model/StudentDTO.java
+++ Modulos/Modulo_04/7.1 ObterDiploma (Base)/src/main/java/com/meli/obterdiploma/model/StudentDTO.java
@@ -3,12 +3,21 @@ package com.meli.obterdiploma.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Getter @Setter
 public class StudentDTO {
+    @NotBlank(message = "O nome do aluno não pode ficar vazio")
+    @Pattern(regexp = "[A-Z]", message = "O nome deve começar com a letra maiúscula")
+    @Size(max = 50, message = "O nome deve ter no máximo 50 caracters")
+
     String studentName;
     String message;
     Double averageScore;
+    @NotEmpty(message = "A lista não pode ser vazia")
     List<SubjectDTO> subjects;
 }
--- Modulos/Modulo_04/7.1 ObterDiploma (Base)/src/main/java/com/meli/obterdiploma/model/SubjectDTO.java
+++ Modulos/Modulo_04/7.1 ObterDiploma (Base)/src/main/java/com/meli/obterdiploma/model/SubjectDTO.java
@@ -3,8 +3,15 @@ package com.meli.obterdiploma.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Getter @Setter
 public class SubjectDTO {
+    @NotEmpty(message = "O nome não pode ser vazio")
+    @Pattern(regexp = "[A-Z]", message = "O assunto tem que começar com a letra maiuscula")
+    @Size(max = 30, message = "Não pode ter mais que 30 caracteres")
     String name;
     Double score;
 }
