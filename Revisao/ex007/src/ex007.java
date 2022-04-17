3824a53ab1f4e495c5887dc0a3ffbe0bd0fd3410
Marcelo <marcelo.leite@mercadolivre.com>
2022-04-17 16:21:41 -0300

    [feat] praticando vetor em java :rocket:

--- Revisao/ex007/src/ex007.java
+++ Revisao/ex007/src/ex007.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class ex007 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-
+    System.out.println("Digite a quantidade de unidade");
     int n = sc.nextInt();
     double[] vect = new double[n];
 
     for (int i = 0; i < n; i++) {
+      System.out.println("Digite a nota");
       vect[i] = sc.nextDouble();
     }
 
@@ -19,6 +20,11 @@ public class ex007 {
     double avg = sum / n;
 
     System.out.printf("A média é: %.2f%n", avg);
+    if (avg <= 6.9) {
+      System.out.println("Aluno reprovado");
+    } else {
+      System.out.println("Aluno aprovado");
+    }
 
     sc.close();
   }
